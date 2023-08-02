85d268765dcdcf9b1bb40679fb20c1e05e0546cf
cx46vt <Nico.van.der.Heide@ing.com>
2023-08-02 16:06:46 +0200

    named all factions

--- src/main/java/data/enums/Faction.java
+++ src/main/java/data/enums/Faction.java
@@ -2,12 +2,26 @@ package data.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 
 @Getter
 @AllArgsConstructor
 public enum Faction {
-    Industrial_Arms("We make the weapons: +1 dmg; Upgrade: Ballistic/Energy", 0, 0, 0, 0, 0, 1);
+    ODEAR,
+    Psight_Ops,
+    Agricultural_League,
+    Industrial_Arms("We make the weapons: +1 dmg; Upgrade: Ballistic/Energy", 0, 0, 0, 0, 0, 1),
+    The_Sanctum,
+    Holy_Ground_Base,
+    Silicon_Syndicate,
+    The_Naturalized,
+    Womens_Defense_Force,
+    Roughnecks("Ignore Hazards; -1 Bureaucracy; Sharp and Tough: +2 Acuity & Fortitude.", 2, 0, 0, 2, 0, 0),
+    Section_Six,
+    Hippocratic_Mercy,
+    The_One_Percent,
+    Dispersed,
+
+    ;
 
     private final String output;
     private final int acuity;
@@ -17,6 +31,16 @@ public enum Faction {
     private final int psionics;
 
     private final int damageBonus;
+
+    Faction() {
+        output = "";
+        acuity = 0;
+        combat = 0;
+        dexterity = 0;
+        fortitude = 0;
+        psionics = 0;
+        damageBonus = 0;
+    }
 
     public String getName() {
         return name().replaceAll("_", " ");
