b7100bc67d7ce2a7714446da93b24de3373a3537
cx46vt <Nico.van.der.Heide@ing.com>
2023-08-02 16:08:17 +0200

    added rating added Advantage enum

--- src/main/java/data/Team.java
+++ src/main/java/data/Team.java
@@ -1,6 +1,6 @@
 package data;
 
-import data.enums.BaseType;
+import data.enums.Advantage;
 import data.enums.BaseUpgrade;
 import data.enums.Faction;
 import lombok.Data;
@@ -10,15 +10,16 @@ import java.util.List;
 @Data
 public class Team {
     private String name;
+    private int rating;
     private Faction faction;
-    private String advantage;
+    private Advantage advantage;
     private Base base;
 
     private Commander commander;
     private List<Member> members;
 
     public int calculateDmg(Person member) {
-        int dmg= faction.getDamageBonus();
+        int dmg = faction.getDamageBonus();
         if (base.getBaseUpgrades().contains(BaseUpgrade.Weapons_Depot)) {
             dmg += 1;
         }
