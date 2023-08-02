1082bce13bf1427dd1b23a6f14c842feb5c10bf6
cx46vt <Nico.van.der.Heide@ing.com>
2023-08-02 16:05:29 +0200

    named all Advantage options

---
+++ src/main/java/data/enums/Advantage.java
@@ -0,0 +1,37 @@
+package data.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Advantage {
+    Advanced_Combat_Training,
+    Armored_Team,
+    Chemically_Enhanced,
+    Connected,
+    Environmental_Specialists,
+    Extra_Gear,
+    High_Alert_Team,
+    Psionic_Talent,
+    Rapid_Response_Team,
+    Ready_to_Respond,
+    Reinforced_Dermal_Layering,
+    Roughest_and_Toughest,
+    Tactical_Command_Team,
+    Vertical_Response_Team,
+    Veteran_Commander,
+    Weapons_Specialists,
+    Well_Armed,
+    ;
+
+    Advantage() {
+        output = "";
+    }
+
+    private final String output;
+
+    public String toString() {
+        return name().replaceAll("_", " ");
+    }
+}
