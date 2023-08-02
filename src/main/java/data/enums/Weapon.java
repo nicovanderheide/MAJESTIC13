a106907e769afe8d5fe720d73be1945490e92b56
cx46vt <Nico.van.der.Heide@ing.com>
2023-08-02 16:07:43 +0200

    added Weapon option

--- src/main/java/data/enums/Weapon.java
+++ src/main/java/data/enums/Weapon.java
@@ -2,16 +2,15 @@ package data.enums;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
 
 @Getter
 @AllArgsConstructor
 public enum Weapon {
-    Pistol("1D6+{dmgBonus}"),
-    Assault_Rifle("2D6+{dmgBonus}"),
-    Sniper_Rifle("1D6+2+{dmgBonus} moving/ 2D6+4+{dmgBonus} stationary"),
-    Heavy_Repeating_Rifle("-2 Combat, 3D6+{dmgBonus}"),
-    ;
+    Pistol("1D6+{dmgBonus}{extra}"),
+    Assault_Rifle("2D6+{dmgBonus}{extra}"),
+    Sniper_Rifle("1D6+2+{dmgBonus} moving/ 2D6+4+{dmgBonus} stationary; {extra}"),
+    Heavy_Repeating_Rifle("-2 Combat, 3D6+{dmgBonus}{extra}"),
+    Flash_Bang_Grenade("+3\" area; FORT 27 Stunned");
 
     private final String output;
 
