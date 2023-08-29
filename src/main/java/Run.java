ae42bb44f9d08dfe302607c09df2ff462223ecec
Nico van der Heide <nico.vanderheide@gmail.com>
2023-08-29 15:30:35 +0200

    1 class to run them all
    and team name fix

---
+++ src/main/java/Run.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Run {
+    public static void main(String[] args) throws IOException {
+        CrewGenerator.main(args);
+        MissionGenerator.main(args);
+    }
+}
